216825ac26cc2b9d2fecee4c7e2db919eae2f3b4
ifthekar <sajalcse2018@gmail.com>
2023-05-23 09:35:20 +0600

    Pattern4

---
+++ pattern4.c
@@ -0,0 +1,19 @@
+/**
+ 1 1 1
+ 0 0
+ 1
+**/
+#include<stdio.h>
+int main(){
+
+    int rows,cols,n;
+    printf("Enter Number of Rows=");
+    scanf("%d",&n);
+    for(rows=n;rows>=1;rows--){
+        for(cols=1;cols<=rows;cols++){
+            printf("%d ",rows%2);
+        }
+        printf("\n");
+    }
+return 0;
+}
