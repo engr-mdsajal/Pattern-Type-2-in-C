961fbb369cf64e0eff04570f6a6e8ec83e8193f2
ifthekar <sajalcse2018@gmail.com>
2023-05-23 11:52:53 +0600

    Pattern 6

---
+++ pattern6.c
@@ -0,0 +1,19 @@
+/**
+ C C C
+ B B
+ A
+**/
+#include<stdio.h>
+int main(){
+
+    int rows,cols,n;
+    printf("Enter Number of Rows=");
+    scanf("%d",&n);
+    for(rows=n;rows>=1;rows--){
+        for(cols=1;cols<=rows;cols++){
+            printf("%c ",rows+64);
+        }
+        printf("\n");
+    }
+return 0;
+}
